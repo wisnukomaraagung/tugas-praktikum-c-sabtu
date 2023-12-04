afa0ffcfd9881bcf20401543c28d778db992ccfe
wisnukomaraagung <wisnukomaraagung@gmail.com>
2023-12-04 12:56:40 +0700

    tugas-praktikum-c-sabtu

---
+++ modul 8/modul 8c.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A[11];
+    int data[] = {12, 15, 7, 10, 5, 2, 17, 25, 9, 20, 35, 28, 14, 11, 999};
+    int length = 14;
+    int count = 0;
+
+    for (int i = 0; i < length && data[i] != 999 && count < 11; i++) {
+        if (data[i] > 9) {  
+            A[count] = data[i];
+            count++;
+        }
+    }
+
+    cout << "Isi array setelah menyimpan nilai > 9 menjadi:" << endl;
+    for (int i = 0; i < count; i++) {
+    	if (i == 0) {
+            cout << "A  ";
+        }
+        cout << A[i] << " ";
+    }
+
+    return 0;
+}
