948a540d0c01f61907eff6f5f5bec62ddbbf4490
wisnukomaraagung <wisnukomaraagung@gmail.com>
2023-12-16 00:13:57 +0700

    tugas-praktikum-c-sabtu

---
+++ modul 10/modul 10b.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A[5] = {12, 8, 17, 5, 15};
+    int B[7] = {5, 11, 7, 25, 9, 12, 14};
+    int C[12] = {0};  
+
+    cout << "Isi Array A: ";
+    for (int I = 0; I < 5; I++) {
+        cout << A[I] << " ";
+    }
+  
+    cout << "\nIsi Array B: ";
+    for (int I = 0; I < 7; I++) {
+        cout << B[I] << " ";
+    }
+
+    int J = 0; 
+
+    for (int I = 0; I < 5; I++) {
+        if (A[I] > 10) {
+            C[J] = A[I];
+            J++;
+        }
+    }
+
+    for (int i = 0; i < 4; i++) {
+        C[J] = 0;
+        J++;
+    }
+
+    for (int I = 6; I >= 0; I--) {
+        if (B[I] > 10) {  
+            C[J] = B[I];
+            J++;
+        }
+    }
+    
+	cout << "\n_______________________________________________________________________________________";	
+    cout << "\nIsi Array C: ";
+    for (int I = 0; I < J; I++) {
+        cout << C[I] << " ";
+    }
+
+    return 0;
+}
